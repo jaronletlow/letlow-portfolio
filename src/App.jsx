8b7b4f10a664d90dc63ae3ee52f6123d93758dda
Jaron <jaronletlow@gmail.com>
2024-06-29 17:23:37 -0700

    Added image and made more fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-		  <h1>This is my portfolio</h1>
+		  <h1>Welcome</h1>
+      <img src="./src/assets/chad.jpg"></img>
     </>
   )
 }
